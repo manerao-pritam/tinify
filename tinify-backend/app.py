56a1fcbb9f88451e32a1263fcef2c9a308f37590
manerao-pritam <manerao.pritam@gmail.com>
2021-05-08 10:30:30 +0100

    added cors, a new route to serve expand request url, and froze the requirements

--- tinify-backend/app.py
+++ tinify-backend/app.py
@@ -27,17 +27,13 @@ def get_base_url(current_request):
                             'http'), headers['host'])
     if 'stage' in current_request.context:
         base_url = '%s/%s' % (base_url, current_request.context.get('stage'))
-    return base_url
 
-
-# landing page
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+    # remove stage name as we are using custom domain name
+    return base_url.replace('/api', '')
 
 
 # shorten then url
-@app.route("/shorten", methods=['POST'])
+@app.route("/shorten", methods=['POST'], cors=True)
 def shorten():
     request_body = app.current_request.json_body
     logger.info(f'Request body: {request_body}')
@@ -72,8 +68,40 @@ def shorten():
         raise BadRequestError("Oops! The url is invalid!")
 
 
+# route to return long url
+@app.route('/expand', methods=['POST'], cors=True)
+def expand():
+    request_body = app.current_request.json_body
+    logger.info(f'Request body: {request_body}')
+
+    try:
+        # get the url hash from requet body
+        urlHash = request_body['url'][-6:]
+        logger.info(f'Received url: {urlHash}')
+
+        response = table.get_item(
+            Key={
+                'urlHash': urlHash
+            }
+        )
+        logger.info(f'DB query result: {response}')
+
+        # get url from urlHash
+        url = response['Item']['url']
+        logger.info(f'Received url: {url}')
+
+        return Response(
+            body=url,
+            status_code=200,
+            headers={'Content-Type': 'application/json'}
+        )
+
+    except:
+        raise BadRequestError("Oops! The url is invalid!")
+
+
 # Route to get the tiny url and then to redirect to the actual url
-@app.route("/{urlHash}")
+@app.route("/{urlHash}", cors=True)
 def redirect(urlHash):
     response = table.get_item(
         Key={
